20a714a5799c45d51979908bd5d82af13f6dd17a
ESAS <teerapongchanon@gmail.com>
2026-01-12 17:15:16 +0700

    update

--- initial.js
+++ initial.js
@@ -2,44 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') });
 const http = require('http');
 const mysql = require("mysql2/promise");
 const axios = require('axios');
+require("dotenv").config(); // ใช้เฉพาะ local
 
 const app = express();
 
 app.use(cors({ origin: '*' }));
-app.use(bodyParser.json({ limit: '500mb', extended: true }));
+app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 
-const PORT = dotenv.parsed.PORT || 3000;
+// Railway จะส่ง PORT มาให้เอง
+const PORT = process.env.PORT || 3000;
 
 // สร้าง HTTP Server
 const httpServer = http.createServer(app);
-httpServer.listen(PORT, () => {
-  console.log(`HTTP Server running on port ${PORT}`);
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 HTTP Server running on port ${PORT}`);
 });
-// httpServer.listen(PORT, "0.0.0.0", () => {
-//   console.log(`🚀 HTTP Server running on:`);
-//   console.log(`   Local:   http://localhost:${PORT}`);
-//   console.log(`   Network: http://swapp.dyndns.org:${PORT}`);
-// });
 
-// สร้าง MySQL Pool
+// สร้าง MySQL Pool (Railway)
 const pool = mysql.createPool({
-  host: dotenv.parsed.DB_HOST,
-  user: dotenv.parsed.DB_USER,
-  password: dotenv.parsed.DB_PASSWORD,
-  database: dotenv.parsed.DB_DATABASE,
-  port: dotenv.parsed.DB_PORT || 3306,
+   host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 // สร้าง Socket.io
-const io = require('socket.io')(httpServer, { 
+const io = require('socket.io')(httpServer, {
   cors: { origin: '*' }
 });
 
